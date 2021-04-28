6a0dc614764b0e8762143a6660a301c809e5cba8
Xtrendence <xtrendence@yahoo.com>
2021-04-28 22:40:36 +0100

    The HttpLib class can now fetch data from a given URL.

--- src/com/xtrendence/aut/HttpLib.java
+++ src/com/xtrendence/aut/HttpLib.java
@@ -1,4 +1,26 @@
 package com.xtrendence.aut;
 
-public class HttpLib {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpLib implements IHttpLib {
+    public Response call(String url) throws Exception {
+        URL urlObject = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) urlObject.openConnection();
+        connection.setRequestMethod("GET");
+
+        int code = connection.getResponseCode();
+
+        if(code != 200) {
+            return new Response(code, null);
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String data = reader.readLine();
+        reader.close();
+
+        return new Response(code, data);
+    }
 }
